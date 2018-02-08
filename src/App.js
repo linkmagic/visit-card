815f527e2284426d8d97fac62be34c32a0229432
Yevhen S <linkmagic@gmail.com>
2018-02-08 15:23:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="AppRoot">
+        <header>
+        </header>
+        <content>
+        </content>
+        <footer>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
