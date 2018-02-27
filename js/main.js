7f79a66c21d727239ff99e03b36dadcfed30fef5
Yevhen S <linkmagic@gmail.com>
2018-02-27 23:11:53 +0200

    routing; refactoring

--- js/main.js
+++
@@ -1,6 +0,0 @@
-function displayMenu() {
-  document.getElementById('idMenuHome').classList.toggle('IconHomeShow');
-  document.getElementById('idMenuContacts').classList.toggle('IconContactsShow');
-  document.getElementById('idMenuExperience').classList.toggle('IconExperienceShow');
-  document.getElementById('idMenuSelfProjects').classList.toggle('IconSelfProjectsShow');
-}
