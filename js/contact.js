a76f7ed9332c5e39cf83b5c97d140450f8ac74a2
Yevhen S <linkmagic@gmail.com>
2018-03-02 23:52:31 +0200

    refactoring; convert tabs to spaces

--- js/contact.js
+++ js/contact.js
@@ -4,9 +4,9 @@ var idUserDataName = document.getElementById('idUserDataName'),
     idContactFormInput = document.getElementById('idContactFormInput');
 
 var userDataObj = {
-	name: '', 
-	email: '',
-	message: ''
+  name: '', 
+  email: '',
+  message: ''
 }
 
 idContactFormInput.placeholder = 'Fill with your name';
@@ -24,29 +24,29 @@ function assignUserData(input, btnId, classNameTitle, userDataObj, fieldName) {
 function fillUserDataForm__Next() {
   // assign name
   if (userDataObj.name.length === 0) {
-  	if (assignUserData(idContactFormInput, idUserDataName, 'ContactForm__UserData__Name', userDataObj, 'name')) {
-  		idContactFormInput.value = '';
-  	  idContactFormInput.placeholder = 'Now your email address';
-  	}
+    if (assignUserData(idContactFormInput, idUserDataName, 'ContactForm__UserData__Name', userDataObj, 'name')) {
+      idContactFormInput.value = '';
+      idContactFormInput.placeholder = 'Now your email address';
+    }
     return;
   }
 
   // assign email
   if (userDataObj.email.length === 0) {
-  	if (assignUserData(idContactFormInput, idUserDataEmail, 'ContactForm__UserData__Email', userDataObj, 'email')) {
-			idContactFormInput.value = '';
-  	  idContactFormInput.placeholder = 'Now write your message :)';
-  	}
+    if (assignUserData(idContactFormInput, idUserDataEmail, 'ContactForm__UserData__Email', userDataObj, 'email')) {
+      idContactFormInput.value = '';
+      idContactFormInput.placeholder = 'Now write your message :)';
+    }
     return;
   }
 
   // assign message
   if (userDataObj.message.length === 0) {
-  	if (assignUserData(idContactFormInput, idUserDataMsg, 'ContactForm__UserData__Msg', userDataObj, 'message')) {
-  		idContactFormInput.value = '';
-  	  idContactFormInput.placeholder = '';
-  	  document.querySelector('.ContactBtn__Submit').focus();
-  	}
+    if (assignUserData(idContactFormInput, idUserDataMsg, 'ContactForm__UserData__Msg', userDataObj, 'message')) {
+      idContactFormInput.value = '';
+      idContactFormInput.placeholder = '';
+      document.querySelector('.ContactBtn__Submit').focus();
+    }
     return;
   }
 
@@ -54,43 +54,43 @@ function fillUserDataForm__Next() {
 
 function inputUserDataKeyPress(event) {
   if (event.keyCode === 13) {
-  	fillUserDataForm__Next();
+    fillUserDataForm__Next();
   }
 }
 
 function openSocial(id) {
-	var siteUrl = '';
-	
-	switch(id) {
-		
-		case 'facebook': {
-			siteUrl = 'https://www.facebook.com/yevhen.sapsai';
-			break;
-		}
+  var siteUrl = '';
+  
+  switch(id) {
+    
+    case 'facebook': {
+      siteUrl = 'https://www.facebook.com/yevhen.sapsai';
+      break;
+    }
 
-		case 'linkedin': {
-			siteUrl = 'https://www.linkedin.com/in/ievgen-sapsay-1a911a5b';
-			break;
-		}
+    case 'linkedin': {
+      siteUrl = 'https://www.linkedin.com/in/ievgen-sapsay-1a911a5b';
+      break;
+    }
 
-		case 'github': {
-			siteUrl = 'https://github.com/linkmagic';
-			break;
-		}
+    case 'github': {
+      siteUrl = 'https://github.com/linkmagic';
+      break;
+    }
 
-		case 'codepen': {
-			siteUrl = 'https://codepen.io/linkmagic';
-			break;
-		}
+    case 'codepen': {
+      siteUrl = 'https://codepen.io/linkmagic';
+      break;
+    }
 
-		default: return;
-	}
-	
-	var win = window.open(siteUrl, '_blank');
-	if (win) {
+    default: return;
+  }
+  
+  var win = window.open(siteUrl, '_blank');
+  if (win) {
     win.focus();
-	} else {
-	  alert('Please allow popups for this website');
-	}
-	
+  } else {
+    alert('Please allow popups for this website');
+  }
+
 }
--- js/preloader.js
+++ js/preloader.js
@@ -1,9 +1,9 @@
 var rotateCube = 0;
 
 function cudeRotation() {
-	if (rotateCube === 360) {
-		rotateCube = 0;
-	}
+  if (rotateCube === 360) {
+    rotateCube = 0;
+  }
   rotateCube++;
   document.querySelector('.cube').style.transform =
     'rotateY(' + rotateCube + 'deg)'+
